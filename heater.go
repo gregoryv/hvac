32c1b1368281c5542408f40561289d10b00683a1
Gregory Vincic <gregoryvse@gmail.com>
2023-11-09 14:47:54 +0100

    Add type Heater

--- heater.go
+++ heater.go
@@ -1,1 +1,7 @@
 package hvac
+
+type Heater struct{}
+
+func (h *Heater) Run() {
+	_ = 1
+}
---
+++ heater_test.go
@@ -0,0 +1,8 @@
+package hvac
+
+import "testing"
+
+func TestHeater_Run(t *testing.T) {
+	var h Heater
+	h.Run()
+}
